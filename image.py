c65344f976e5c39a9c89261621c467de41371a5c
Josiah <josiah_g@live.com>
2023-07-06 19:49:04 -0400

    Formatting

--- image.py
+++ image.py
@@ -1,38 +1,39 @@
 import requests
-import shutil 
+import shutil
 import openai
 
-class Image():
+
+class Image:
     def __init__(self):
         self.image_url = ""
         self.image_file_name = ""
         self.image_prompt = ""
         self.image_response = ""
-    
+
     # Create a prompt for the recipe image based on the recipe title
     def create_image_prompt(self, recipe_title):
-        self.image_prompt = f"{recipe_title}, professional food photography, 15mm, studio lighting"
-    
+        self.image_prompt = (
+            f"{recipe_title}, professional food photography, 15mm, studio lighting"
+        )
+
     def create_image(self):
         self.image_response = openai.Image.create(
             prompt=self.image_prompt,
-            n = 1, # number of images to return
-            size = '1024x1024', # size of image
-            )
-    
+            n=1,  # number of images to return
+            size="1024x1024",  # size of image
+        )
+
     def set_image_url(self):
-        self.image_url = self.image_response['data'][0]['url']
-        
-        
+        self.image_url = self.image_response["data"][0]["url"]
+
     def get_image_url(self):
         return self.image_url
-    
-    
+
     # Method to save image to local directory
     def save_image(self, filename):
         image_res = requests.get(self.image_url, stream=True)
         if image_res.status_code == 200:
-            with open(filename, 'wb') as f:
+            with open(filename, "wb") as f:
                 image_res.raw.decode_content = True
                 shutil.copyfileobj(image_res.raw, f)
             del image_res
@@ -40,5 +41,3 @@ class Image():
         else:
             print("Error loading image")
             return False
-        
-        
--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from recipe import Recipe
 from image import Image
+
+
 def main():
-    
+
     # Create recipe object
     recipe = Recipe()
-    
+
     # Create AI Image object
-    
+
     image = Image()
-    
-    st.title('👨‍🍳 OPENAI RECIPE GENERATOR 👩‍🍳')
-    # Ingredients input 
-    ingredients = st.text_input('Enter your ingredients separated by commas')
+
+    st.title("👨‍🍳 OPENAI RECIPE GENERATOR 👩‍🍳")
+    # Ingredients input
+    ingredients = st.text_input("Enter your ingredients separated by commas")
 
     # Button
-    if st.button('Submit'):
+    if st.button("Submit"):
         # Check if ingredients are entered
         if not ingredients or not ingredients.strip():
             st.warning("Please enter some ingredients")
@@ -33,19 +35,16 @@ def main():
             # Create image from prompt using Dalle API
             image.create_image()
             image.set_image_url()
-            
+
             # Display recipe
             st.write(recipe.response.choices[0].text)
             # Display image
-            st.image(image.get_image_url(), caption=f"AI generated image of {recipe.get_recipe_title()}", use_column_width=True)
-            
-        
-        
-    
-
-
+            st.image(
+                image.get_image_url(),
+                caption=f"AI generated image of {recipe.get_recipe_title()}",
+                use_column_width=True,
+            )
 
 
 if __name__ == "__main__":
     main()
-        
